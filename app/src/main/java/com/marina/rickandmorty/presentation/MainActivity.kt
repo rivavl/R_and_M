6c1ed6414ea1be66c151b1610e17768b35e26068
rivavl <mvkalinenkova@yandex.ru>
2022-07-05 20:07:48 +0300

    Presentation

--- app/src/main/java/com/marina/rickandmorty/presentation/MainActivity.kt
+++ app/src/main/java/com/marina/rickandmorty/presentation/MainActivity.kt
@@ -2,15 +2,45 @@ package com.marina.rickandmorty.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.marina.rickandmorty.R
-import com.marina.rickandmorty.presentation.character.CharactersListFragment
+import com.marina.rickandmorty.presentation.character.list.CharactersListFragment
+import com.marina.rickandmorty.presentation.episode.list.EpisodesListFragment
+import com.marina.rickandmorty.presentation.location.list.LocationsListFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        launchFragment(CharactersListFragment())
+        findViewById<BottomNavigationView>(R.id.bottom_nav_bar).setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.characters_menu -> {
+                    launchFragment(CharactersListFragment())
+                    true
+                }
+
+                R.id.locations_menu -> {
+                    launchFragment(LocationsListFragment())
+                    true
+                }
+
+                R.id.episodes_menu -> {
+                    launchFragment(EpisodesListFragment())
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+    }
+
+    private fun launchFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
-            .add(R.id.container, CharactersListFragment())
+            .replace(R.id.container, fragment)
             .commit()
     }
 }
